8e7fa0a2ea3286393f0f1a171c0dbd375122de17
seneark <seneark@protonmail.com>
2020-08-18 11:33:34 +0530

    Updates

---
+++ precache-manifest.be79c8089c601d1fb62835af1f0bad7b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ba98857b184fcf1fd6e4c080a2160",
+    "url": "/Fliqaindia/index.html"
+  },
+  {
+    "revision": "1dfa374db269073695ba",
+    "url": "/Fliqaindia/static/css/2.dcf1a2d3.chunk.css"
+  },
+  {
+    "revision": "f4de1ba8ec56d0f82de8",
+    "url": "/Fliqaindia/static/css/main.5b9fd2eb.chunk.css"
+  },
+  {
+    "revision": "1dfa374db269073695ba",
+    "url": "/Fliqaindia/static/js/2.c27c6408.chunk.js"
+  },
+  {
+    "revision": "ac708def751a8292e55daf5717e57737",
+    "url": "/Fliqaindia/static/js/2.c27c6408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4de1ba8ec56d0f82de8",
+    "url": "/Fliqaindia/static/js/main.fbc97f52.chunk.js"
+  },
+  {
+    "revision": "c1a8f972023eb22d7ff2",
+    "url": "/Fliqaindia/static/js/runtime-main.4ba47f3e.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Fliqaindia/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Fliqaindia/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Fliqaindia/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Fliqaindia/static/media/slick.f97e3bbf.svg"
+  }
+]);
